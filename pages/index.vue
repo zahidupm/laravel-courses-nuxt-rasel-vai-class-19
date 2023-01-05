5dd4f1847af20f4b63d82b162633d0a53468f5fb
zahidupm <zahidhasanlimon111@gmail.com>
2023-01-05 23:39:25 +0600

    data lazyfetch

--- pages/index.vue
+++ pages/index.vue
@@ -6,8 +6,16 @@
   <ul>
     <li v-for="user in users" :key="user.id">{{ user.name }}</li>
   </ul>
+
+  <p v-if="loading">Loading...</p>
+  <ul v-else>
+    <li v-for="user in user2">{{ user.name }}</li>
+  </ul>
 </template>
 
 <script setup>
 const users = await $fetch("https://jsonplaceholder.typicode.com/users");
+const { loading, data: user2 } = await useLazyFetch(
+  "https://jsonplaceholder.typicode.com/users"
+);
 </script>
