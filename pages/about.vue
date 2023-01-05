f3dbaea3d22cd38c5397b5792de8722ad8699277
zahidupm <zahidhasanlimon111@gmail.com>
2023-01-05 23:27:24 +0600

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h2>About</h2>
+    <NuxtLink to="/">Back to home</NuxtLink>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "about",
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h2>Home</h2>
+    <NuxtLink to="/about"></NuxtLink>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "index",
+};
+</script>
+
+<style lang="scss" scoped></style>
