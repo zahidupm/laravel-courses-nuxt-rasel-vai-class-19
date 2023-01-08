4b3108442d9b9b50d9080965a7a41b1d5483b418
zahidupm <zahidhasanlimon111@gmail.com>
2023-01-08 14:55:38 +0600

    class 19 desing

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,7 @@
-<template>
-  <div>
-    <h2>Home</h2>
-    <slot />
-  </div>
-
-  <p v-if="loading">Loading...</p>
-  <ul v-else>
-    <li v-for="user in user2">{{ user.name }}</li>
-  </ul>
-</template>
+<template></template>
 
 <script setup>
-const users = await $fetch("https://jsonplaceholder.typicode.com/users");
+// const users = await $fetch("https://jsonplaceholder.typicode.com/users");
 const { loading, data: user2 } = await useLazyFetch(
   "https://jsonplaceholder.typicode.com/users"
 );
